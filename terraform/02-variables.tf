7a7979714163b0f670c238af7f1e177a4f00502c
Paul Bordea <paul.bordea@emeal.nttdata.com>
2024-03-22 14:24:14 +0200

    Extract sensitive data in tfvars file

--- terraform/02-variables.tf
+++ terraform/02-variables.tf
@@ -19,20 +19,17 @@ variable "aks_name" {
 
 # SSH Public Key for Linux VMs
 variable "ssh_public_key" {
-  default = "~/.ssh/aks-prod-sshkeys-terraform/aksprodsshkey.pub"
   description = "This variable defines the SSH Public Key for Linux k8s Worker nodes"  
 }
 
 # Windows Admin Username for k8s worker nodes
 variable "windows_admin_username" {
   type = string
-  default = "azureuser"
   description = "This variable defines the Windows admin username k8s Worker nodes"  
 }
 
 # Windows Admin Password for k8s worker nodes
 variable "windows_admin_password" {
   type = string
-  default = "xxx"  # Can be set up inside a local terraform.tfvars file
   description = "This variable defines the Windows admin password k8s Worker nodes"  
 }
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+ssh_public_key         = "~/.ssh/aks-prod-sshkeys-terraform/aksprodsshkey.pub"
+azureuser              = "azureuser"
+windows_admin_password = "xxxxx"
